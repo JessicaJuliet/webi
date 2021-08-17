defdef1e34178902060ff5f757a32c1be0aee455
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-08-17 21:10:16 +0000

    Remove Secret Key

--- webi/settings.py
+++ webi/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&wjfd_+6w3%e3w82)el!v%54hbojjjnp_*ud8l$g%f*n910@=r'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
