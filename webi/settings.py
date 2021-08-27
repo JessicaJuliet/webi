74e18c313de644f3502ff6600937f29817729ba4
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-08-27 19:12:21 +0000

    Link base.css, upload hero image and add to homepage

--- webi/settings.py
+++ webi/settings.py
@@ -122,8 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
