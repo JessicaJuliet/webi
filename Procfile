fa03ab89319449441e4221582e125158c6700ae6
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-08-28 16:19:45 +0000

    Add Procfile and requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn webi.wsgi:application
--- webi/settings.py
+++ webi/settings.py
@@ -79,12 +79,17 @@ WSGI_APPLICATION = 'webi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        }
-}
+if 'DATABASES_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
