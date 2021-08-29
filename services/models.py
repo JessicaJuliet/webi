e55803aabbcc8a8457da36374abf5f7247deed0f
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-08-29 12:59:11 +0000

    Add extra field to category model in services app

--- services/models.py
+++ services/models.py
@@ -7,6 +7,7 @@ class Category(models.Model):
     """
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    type = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
