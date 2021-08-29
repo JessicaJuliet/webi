7cd4392cac21a5153b060593cf1160aaf04a3d4b
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-08-29 15:38:21 +0000

    Fix pylint errors in checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fileds = ('order_numer', 'date',
+                       'order_total', 'grand_total')
+
+    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number',
+              'country', 'postcode', 'town_or_city', 'street_address',
+              'county', 'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
