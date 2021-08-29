dd26e3d4dee802050bb8887b0ca7c7dfb90b68c7
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-08-29 12:33:20 +0000

    Create Category, Bundles, Addons & Images models in Services app

--- services/models.py
+++ services/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class Category (models.Model):
+    """
+    Category model to filter services by type
+    """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Bundles(models.Model):
+    """
+    Bundles model to display bundles information
+    """
+    Category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Addons(models.Model):
+    """
+    Addons model to display add-ons information
+    """
+    Category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Images(models.Model):
+    """
+    Images model to display services images
+    """
+    name = models.CharField(max_length=254)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
