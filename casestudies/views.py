b14baebed656f9109866a4bf7921b63ddcdc5daa
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-05 15:01:55 +0000

    Add casestudies from json to template and style in css

--- casestudies/views.py
+++ casestudies/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Casestudy
 
 
 def casestudies(request):
     """ A view to return the case studies page """
-    return render(request, 'casestudies/casestudies.html')
+    casestudies = Casestudy.objects.all()
+    context = {
+        'casestudies': casestudies,
+    }
+
+    return render(request, 'casestudies/casestudies.html', context)
