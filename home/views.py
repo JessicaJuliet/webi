c52f9f3fbf133a45063c5652508c7dafc34dfd86
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-05 16:55:34 +0000

    Add casestudies view to home and render featured studys on homepage

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+# Resource: https://stackoverflow.com/questions/31406662/django-unable-to-import-model-from-another-app
+from casestudies.models import Casestudy
 
 # Create your views here.
 
 
 def index(request):
     """A view to return the homepage (index)"""
-    return render(request, 'home/index.html')
+    casestudies = Casestudy.objects.all()
+    context = {
+        'casestudies': casestudies,
+    }
+
+    return render(request, 'home/index.html', context)
