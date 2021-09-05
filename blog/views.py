baac1700b712c1f549059a0d68229a136eef73e8
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-05 15:49:26 +0000

    Add json blog data to blog template and style in css

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Blog
 
 
 def blog(request):
     """ A view to return the blog page """
-    return render(request, 'blog/blog.html')
+    blogs = Blog.objects.all()
+    context = {
+        'blogs': blogs,
+    }
+    return render(request, 'blog/blog.html', context)
