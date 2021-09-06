a1f741e2a4298eabdcf862356636dd1287511af4
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-06 19:30:08 +0000

    Create individual blog post pages and link to json data

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name='blog')
+    path('', views.blog, name='blog'),
+    path('<slug:slug>/', views.blog_post, name='blog_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 
 def blog(request):
     """ A view to return the blog page """
+
     blogs = Blog.objects.all()
+
     context = {
         'blogs': blogs,
     }
     return render(request, 'blog/blog.html', context)
+
+
+def blog_post(request, slug):
+    """ A view to show individual blog post pages """
+
+    blog = get_object_or_404(Blog, slug=slug)
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, 'blog/blog_post.html', context)
