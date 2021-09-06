07657f9bb3ebb34d99860be6715b9a514a8b3e68
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-06 17:27:34 +0000

    Create new view to show individual case study pages

--- casestudies/urls.py
+++ casestudies/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.casestudies, name='casestudies')
+    path('', views.casestudies, name='casestudies'),
+    path('<slug:slug>/', views.casestudy_content, name='casestudy_content'),
 ]
--- casestudies/views.py
+++ casestudies/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Casestudy
 
 
 def casestudies(request):
     """ A view to return the case studies page """
+
     casestudies = Casestudy.objects.all()
+
     context = {
         'casestudies': casestudies,
     }
 
     return render(request, 'casestudies/casestudies.html', context)
+
+
+def casestudy_content(request, slug):
+    """ A view to show individual case study pages """
+
+    casestudy = get_object_or_404(Casestudy, slug=slug)
+
+    context = {
+        'casestudy': casestudy,
+    }
+
+    return render(request, 'casestudies/casestudy_content.html', context)
