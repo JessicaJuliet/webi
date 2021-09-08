242cff115a926edadf9382c78fd55e39928316cd
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-08 20:13:05 +0000

    Create addon json file and link and style content in css

--- services/views.py
+++ services/views.py
@@ -5,4 +5,9 @@ from .models import Category, Bundle, Addon, Image, Type
 def addons(request):
     """ A view to return the Add-ons page """
 
-    return render(request, 'services/addons.html')
+    addons = Addon.objects.all()
+
+    context = {
+        'addons': addons,
+    }
+    return render(request, 'services/addons.html', context)
