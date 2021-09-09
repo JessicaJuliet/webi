3742e0ece6d02ce5740f2afaa3d5aa80a25ca8b1
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-09 18:23:51 +0000

    Add filter menu link to addons page and update in views

--- services/views.py
+++ services/views.py
@@ -6,8 +6,17 @@ def addons(request):
     """ A view to return the Add-ons page """
 
     addons = Addon.objects.all()
+    #types = None
+
+    #if request.GET:
+    #    if 'type' in request.GET:
+    #        types = request.GET['type'].split(',')
+    #        addons = addons.filter(type__name__in=types)
+    #        types = Type.objects.filter(name__in=types)
 
     context = {
         'addons': addons,
+    #    'current_types': type,
     }
+
     return render(request, 'services/addons.html', context)
