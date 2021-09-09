6f5931d00d1802908b613f6f834baa7bf5d95af3
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-09 20:30:15 +0000

    Remove unused collectstatic

--- webi/settings.py
+++ webi/settings.py
@@ -26,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = os.environ.get('DEVELOPMENT')
+# DEBUG = True
 
 ALLOWED_HOSTS = ['webi-development.herokuapp.com', 'localhost']
 
@@ -139,10 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/ckeditor')
 
-CKEDITOR_BASEPATH = "static/ckeditor"
+# CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 
 # Resource: Static root - https://groups.google.com/g/django-users/c/jrDZ-dz_gfU?pli=1, https://newbedev.com/collecting-staticfiles-throws-improperlyconfigured
 # Default primary key field type
