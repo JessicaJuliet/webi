fb335e932f1fe5e223599ca11c31afb81752f458
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-11 10:52:42 +0000

    Populate About page with content

--- about/views.py
+++ about/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from casestudies.models import Casestudy
 
 
 def about(request):
     """ A view to return the about page """
-    return render(request, 'about/about.html')
+    casestudies = Casestudy.objects.all()
+    context = {
+        'casestudies': casestudies,
+    }
+
+    return render(request, 'about/about.html', context)
