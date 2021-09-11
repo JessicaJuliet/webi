7b807d14954d9f977262ef5b940f7956ad963dbb
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-11 10:18:11 +0000

    Fix bug which prevents addon filter working

--- services/views.py
+++ services/views.py
@@ -3,20 +3,20 @@ from .models import Category, Bundle, Addon, Image, Type
 
 
 def addons(request):
-    """ A view to return the Add-ons page """
+    """ A view to return the Add-ons page and filter addons by type """
 
     addons = Addon.objects.all()
-    #types = None
+    types = None
 
-    #if request.GET:
-    #    if 'type' in request.GET:
-    #        types = request.GET['type'].split(',')
-    #        addons = addons.filter(type__name__in=types)
-    #        types = Type.objects.filter(name__in=types)
+    if request.GET:
+        if 'type' in request.GET:
+            types = request.GET['type'].split(',')
+            addons = addons.filter(type__name__in=types)
+            types = Type.objects.filter(name__in=types)
 
     context = {
         'addons': addons,
-    #    'current_types': type,
+        'current_types': type,
     }
 
     return render(request, 'services/addons.html', context)
