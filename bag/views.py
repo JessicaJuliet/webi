76bb096a2faa1916b48f8e035f242be07663a054
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-16 18:07:09 +0000

    Add toasts messages

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
-from services.models import Image
+from django.contrib import messages
+
+from services.models import Addon, Bundle
 
 
 def view_bag(request):
@@ -11,6 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    addon = Addon.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -19,6 +22,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {addon.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
--- webi/settings.py
+++ webi/settings.py
@@ -91,6 +91,9 @@ TEMPLATES = [
     },
 ]
 
+# Store toast messages in the session
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
