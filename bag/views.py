9b7069451b82a24f66450101f661667e38524231
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-16 18:29:55 +0000

    Add notification css, shopping bag preview and additional messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from services.models import Addon, Bundle
@@ -20,6 +20,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {addon.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {addon.name} to your bag')
@@ -31,13 +32,16 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    addon = get_object_or_404(Addon, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {addon.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {addon.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -46,8 +50,10 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
+    addon = get_object_or_404(Addon, pk=item_id)
     bag = request.session.get('bag', {})
     bag.pop(item_id)
+    messages.success(request, f'Removed {addon.name} from your bag')
 
     request.session['bag'] = bag
     return HttpResponse(status=200)
