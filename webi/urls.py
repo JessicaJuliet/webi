fa82353cd49257c3b30d73efe38a0fe4d196c1bb
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-21 16:47:22 +0000

    Create profiles app urls and viewg

--- webi/urls.py
+++ webi/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('services/', include('services.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 """ Static function to add MEDIA_URL to list of URLs """
