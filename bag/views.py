fcbae4d0d442ed5355f4b95b6dfd124ffdaa5672
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-22 19:53:17 +0000

    Update bag and services apps

--- bag/views.py
+++ bag/views.py
@@ -10,50 +10,61 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 
-def add_to_bag(request, item_id):
+def add_to_bag(request, item_id, is_addon):
     """ Add a quantity of the specified product to the shopping bag """
 
-    addon = Addon.objects.get(pk=item_id)
+    if is_addon == 'True':
+        service = Addon.objects.get(pk=item_id)
+    else:
+        service = Bundle.objects.get(pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-        messages.success(request, f'Updated {addon.name} quantity to {bag[item_id]}')
+        messages.success(request, f'Updated {service.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
-        messages.success(request, f'Added {addon.name} to your bag')
+        messages.success(request, f'Added {service.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
 
 
-def adjust_bag(request, item_id):
+def adjust_bag(request, item_id, is_addon):
     """Adjust the quantity of the specified product to the specified amount"""
 
-    addon = get_object_or_404(Addon, pk=item_id)
+    if is_addon == 'True':
+        service = get_object_or_404(Addon, pk=item_id)
+    else:
+        service = get_object_or_404(Bundle, pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
-        messages.success(request, f'Updated {addon.name} quantity to {bag[item_id]}')
+        messages.success(request, f'Updated {service.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
-        messages.success(request, f'Removed {addon.name} from your bag')
+        messages.success(request, f'Removed {service.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
 
-def remove_from_bag(request, item_id):
+def remove_from_bag(request, item_id, is_addon):
     """Remove the item from the shopping bag"""
+    if is_addon == 'True':
+        service = get_object_or_404(Addon, pk=item_id)
+    else:
+        service = get_object_or_404(Bundle, pk=item_id)
 
-    addon = get_object_or_404(Addon, pk=item_id)
     bag = request.session.get('bag', {})
     bag.pop(item_id)
-    messages.success(request, f'Removed {addon.name} from your bag')
+    messages.success(request, f'Removed {service.name} from your bag')
 
     request.session['bag'] = bag
     return HttpResponse(status=200)
