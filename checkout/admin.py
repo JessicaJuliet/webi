90297de164698e6fd877c37e4cab284616c5d672
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-24 19:56:36 +0000

    Add order history and update checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -15,7 +15,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'order_total', 'grand_total', 
                        'original_bag', 'stripe_pid',)
 
-    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number',
+    fields = ('order_number', 'user_profile', 'date', 'full_name', 'email', 'phone_number',
               'country', 'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'order_total', 'grand_total', 
               'original_bag', 'stripe_pid',)
