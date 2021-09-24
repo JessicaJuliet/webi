8eb41a9a95a747c10a4366adc291a712f2d956a2
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-24 20:55:43 +0000

    Create a product form for the product admin

---
+++ services/forms.py
@@ -0,0 +1,19 @@
+
+from django import forms
+from .models import Addon, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Addon
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'custom-button'
