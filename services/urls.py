79a652639bad53f8925d220182a8fa29413142ba
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-25 12:23:07 +0000

    Create service detail template

--- services/urls.py
+++ services/urls.py
@@ -5,13 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_services, name='services'),
+    path('<service_id>', views.service_detail, name='service_detail'),
     path('bundles/', views.bundles, name='bundles'),
     path('<slug:slug>/', views.bundle_details, name='bundle_details'),
     path('add', views.add_product, name='add_product'),
 ]
-
-
-"""   path('addons/', views.addons, name='addons'),
-    path('bundles/', views.bundles, name='bundles'),
-    path('<slug:slug>/', views.bundle_details, name='bundle_details'),
-    path('add', views.add_product, name='add_product'),"""
--- services/views.py
+++ services/views.py
@@ -24,6 +24,18 @@ def all_services(request):
     return render(request, 'services/services.html', context)
 
 
+def service_detail(request, service_id):
+    """ A view to show individual service details """
+
+    service = get_object_or_404(Addon, pk=service_id)
+
+    context = {
+        'service': service,
+    }
+
+    return render(request, 'services/service_detail.html', context)
+
+
 def bundles(request):
     """ A view to render the Bundles page """
 
