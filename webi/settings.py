41a7204d7745f6d249d2393a371724c03f0f0a55
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-09-29 20:28:32 +0000

    Fix typo in settings.py

--- webi/settings.py
+++ webi/settings.py
@@ -130,7 +130,7 @@ WSGI_APPLICATION = 'webi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASES_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
