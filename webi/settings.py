e2d2889a777d8d1f91946be81aaa351596536015
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-10-02 10:00:02 +0000

    Update casestudy urls

--- webi/settings.py
+++ webi/settings.py
@@ -134,6 +134,7 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+
 else:
     DATABASES = {
         'default': {
