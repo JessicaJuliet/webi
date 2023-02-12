879666aa4fbd78599250af3c136a040ca0813b6d
JessicaJuliet <jessica.akomfrah@gmail.com>
2023-02-12 18:07:38 +0000

    Add build file & Render to settings.py

--- webi/settings.py
+++ webi/settings.py
@@ -31,6 +31,11 @@ DEBUG = os.environ.get('DEVELOPMENT')
 
 ALLOWED_HOSTS = ['webi-development.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
